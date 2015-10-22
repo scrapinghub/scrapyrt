2d498a1704c2e08074c432d68527be69c911edbf
Simon Hewitt <si@sjhewitt.co.uk>
2015-10-22 13:38:04 -0700

    specific demjson version not required

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         'Twisted>=14.0.0',
         'Scrapy>=0.24.0',
-        'demjson>=2.2.3'
+        'demjson'
     ],
     package_data={
         'scrapyrt': [
