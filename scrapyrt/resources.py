a1fa6fda8761fa0b1012d728f32c5d744a2935eb
Pawel Miech <pawelmhm@gmail.com>
2016-12-22 12:24:49 +0100

    [resources] refactor, rename important variable names
    request_data will be changed to api_params;
    spider_data will be changed to scrapy_request_args

--- scrapyrt/resources.py
+++ scrapyrt/resources.py
@@ -117,14 +117,15 @@ class CrawlResource(ServiceResource):
         At the moment kwargs for scrapy request are not supported in GET.
         They are supported in POST handler.
         """
-        request_data = dict(
+        api_params = dict(
             (name.decode('utf-8'), value[0].decode('utf-8'))
             for name, value in request.args.items()
         )
-        spider_data = extract_scrapy_request_args(request_data, raise_error=False)
-        request_data = self._get_api_params(request_data)
-        self.validate_options(spider_data, request_data)
-        return self.prepare_crawl(request_data, spider_data, **kwargs)
+        scrapy_request_args = extract_scrapy_request_args(api_params,
+                                                          raise_error=False)
+        api_params = self._get_api_params(api_params)
+        self.validate_options(scrapy_request_args, api_params)
+        return self.prepare_crawl(api_params, scrapy_request_args, **kwargs)
 
     def render_POST(self, request, **kwargs):
         """
@@ -144,23 +145,25 @@ class CrawlResource(ServiceResource):
         """
         request_body = request.content.getvalue()
         try:
-            request_data = demjson.decode(request_body)
+            api_params = demjson.decode(request_body)
         except ValueError as e:
             message = "Invalid JSON in POST body. {}"
             message.format(e.pretty_description())
             raise Error('400', message=message)
 
-        log.msg("{}".format(request_data))
+        log.msg("{}".format(api_params))
         try:
-            spider_data = extract_scrapy_request_args(request_data.get("request", {}), raise_error=True)
+            scrapy_request_args = extract_scrapy_request_args(
+                api_params.get("request", {}), raise_error=True
+            )
         except ValueError as e:
             raise Error(400, e.message)
 
-        if not request_data.get("start_requests"):
-            self.get_required_argument(request_data, "request")
-        request_data = self._get_api_params(request_data)
-        self.validate_options(spider_data, request_data)
-        return self.prepare_crawl(request_data, spider_data, **kwargs)
+        if not api_params.get("start_requests"):
+            self.get_required_argument(api_params, "request")
+        api_params = self._get_api_params(api_params)
+        self.validate_options(scrapy_request_args, api_params)
+        return self.prepare_crawl(api_params, scrapy_request_args, **kwargs)
 
     def _get_api_params(self, request_data):
         api_params = {}
@@ -169,19 +172,20 @@ class CrawlResource(ServiceResource):
                 api_params[k] = v
         return api_params
 
-    def validate_options(self, spider_data, api_params):
-        url = spider_data.get("url")
+    def validate_options(self, scrapy_request_args, api_params):
+        url = scrapy_request_args.get("url")
         start_requests = api_params.get("start_requests")
         if not url and not start_requests:
-            raise Error(400, "'url' is required if start_requests are disabled")
+            raise Error(400,
+                        "'url' is required if start_requests are disabled")
 
-    def get_required_argument(self, request_data, name, error_msg=None):
+    def get_required_argument(self, api_params, name, error_msg=None):
         """Get required API key from dict-like object.
 
-        :param dict request_data:
+        :param dict api_params:
             dictionary with names and values of parameters supplied to API.
         :param str name:
-            required key that must be found in request_data
+            required key that must be found in api_params
         :return: value of required param
         :raises Error: Bad Request response
 
@@ -189,17 +193,17 @@ class CrawlResource(ServiceResource):
         if error_msg is None:
             error_msg = 'Missing required parameter: {}'.format(repr(name))
         try:
-            value = request_data[name]
+            value = api_params[name]
         except KeyError:
             raise Error('400', message=error_msg)
         if not value:
             raise Error('400', message=error_msg)
         return value
 
-    def prepare_crawl(self, request_data, spider_data, *args, **kwargs):
+    def prepare_crawl(self, api_params, spider_data, *args, **kwargs):
         """Schedule given spider with CrawlManager.
 
-        :param dict request_data:
+        :param dict api_params:
             arguments needed to find spider and set proper api parameters
             for crawl (max_requests for example)
 
@@ -207,16 +211,17 @@ class CrawlResource(ServiceResource):
             should contain positional and keyword arguments for Scrapy
             Request object that will be created
         """
-        spider_name = self.get_required_argument(request_data, 'spider_name')
-        start_requests = request_data.get("start_requests", False)
+        spider_name = self.get_required_argument(api_params, 'spider_name')
+        start_requests = api_params.get("start_requests", False)
         try:
-            max_requests = request_data['max_requests']
+            max_requests = api_params['max_requests']
         except (KeyError, IndexError):
             max_requests = None
         dfd = self.run_crawl(
-            spider_name, spider_data, max_requests, start_requests=start_requests, *args, **kwargs)
+            spider_name, spider_data, max_requests,
+            start_requests=start_requests, *args, **kwargs)
         dfd.addCallback(
-            self.prepare_response, request_data=request_data, *args, **kwargs)
+            self.prepare_response, request_data=api_params, *args, **kwargs)
         return dfd
 
     def run_crawl(self, spider_name, spider_data,
