a272cd62cdd95a1064b5ba81179cbce8ac6a6b26
Pawel Miech <pawelmhm@gmail.com>
2016-12-22 14:57:43 +0100

    [logging] spider log filename format
    adds tests; adds format string that can be customized from settings.

--- scrapyrt/core.py
+++ scrapyrt/core.py
@@ -164,7 +164,8 @@ class CrawlManager(object):
         log_dir = os.path.join(self.log_dir, self.spider_name)
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
-        filename = datetime.datetime.now().strftime('%Y-%m-%dT%H;%M;%S.%f') + '.log'
+        time_format = settings.SPIDER_LOGFILE_TIMEFORMAT
+        filename = datetime.datetime.now().strftime(time_format) + '.log'
         return os.path.join(log_dir, filename)
 
     def get_project_settings(self):
