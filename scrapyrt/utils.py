0da27f2749b1d16ad712f0f41080d0d0f1d19ba6
Pawel Miech <pawelmhm@gmail.com>
2016-12-22 12:59:24 +0100

    [utils] add tests for extract_scrapy_request_args
    and also add docstring for that

--- scrapyrt/utils.py
+++ scrapyrt/utils.py
@@ -3,6 +3,13 @@ from scrapy import Request
 
 
 def extract_scrapy_request_args(dictionary, raise_error=False):
+    """
+    :param dictionary: Dictionary with parameters passed to API
+    :param raise_error: raise ValueError if key is not valid arg for
+                        scrapy.httpRequest
+    :return: dictionary of valid scrapy.http.Request positional and keyword
+            arguments.
+    """
     result = dictionary.copy()
     args = inspect.getargspec(Request.__init__).args
     for key in dictionary.keys():
---
+++ tests/test_utils.py
@@ -0,0 +1,34 @@
+import re
+
+import pytest
+
+from scrapyrt.utils import extract_scrapy_request_args
+
+
+class TestUtils(object):
+    def test_get_scrapy_request_args(self):
+        args = {
+            "url": "http://foo.com",
+            "callback": "parse",
+            "noise": True
+        }
+
+        result = extract_scrapy_request_args(args)
+
+        assert result["url"] == "http://foo.com"
+        assert result["callback"] == "parse"
+        assert "noise" not in result
+
+    def test_get_scrapy_request_args_error(self):
+        args = {
+            "url": "http://foo.com",
+            "callback": "parse",
+            "noise": True
+        }
+
+        with pytest.raises(ValueError) as e:
+            extract_scrapy_request_args(args, raise_error=True)
+
+        expected_msg =u"'noise' is not a valid argument for scrapy.Request"
+        assert re.search(expected_msg, e.value.message)
+
