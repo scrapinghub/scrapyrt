244e1d613481fc4de121c5a5d9d464a6db54694e
Pawel Miech <pawelmhm@gmail.com>
2017-02-10 15:41:36 +0100

    [tox] add environment with scrapy 1.0.0 and python 2.7

--- scrapyrt/log.py
+++ scrapyrt/log.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
+import six
 from logging.config import dictConfig
 import logging
 import os
 import sys
 
-from scrapy.utils.python import to_bytes
+try:
+    from scrapy.utils.python import to_bytes
+except ImportError:
+    def to_bytes(text, encoding=None, errors='strict'):
+        """Return the binary representation of `text`. If `text`
+        is already a bytes object, return it as-is."""
+        if isinstance(text, bytes):
+            return text
+        if not isinstance(text, six.string_types):
+            raise TypeError('to_bytes must receive a unicode, str or bytes '
+                            'object, got %s' % type(text).__name__)
+        if encoding is None:
+            encoding = 'utf-8'
+        return text.encode(encoding, errors)
+
 from twisted.python import log
 from twisted.python.log import startLoggingWithObserver
 from twisted.python.logfile import DailyLogFile
