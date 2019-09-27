aee93eed2e459d5faab3e6e1c65abe57d79967ba
Pawel Miech <pawelmhm@gmail.com>
2019-09-27 15:05:58 +0200

    [core] minor whitespace cleanup

--- scrapyrt/core.py
+++ scrapyrt/core.py
@@ -105,10 +105,9 @@ class CrawlManager(object):
         # callback will be added after instantiation of crawler object
         # because we need to know if spider has method available
         self.callback_name = request_kwargs.pop('callback', None) or 'parse'
-
         # do the same for errback
         self.errback_name = request_kwargs.pop('errback', None) or 'parse'
-        
+
         if request_kwargs.get("url"):
             self.request = self.create_spider_request(deepcopy(request_kwargs))
         else:
@@ -150,7 +149,7 @@ class CrawlManager(object):
 
         Schedule request for url given to api, with optional callback
         and errback that can be passed as GET parameter.
-        
+
         spider_idle signal is used because we want to optionally enable
         start_requests for the spider and if request is scheduled in
         spider_opened signal handler it's fired earlier then start_requests
@@ -165,7 +164,6 @@ class CrawlManager(object):
             errback = getattr(self.crawler.spider, self.errback_name)
             assert callable(errback), 'Invalid errback'
             self.request = self.request.replace(errback=errback)
-           
             modify_request = getattr(
                 self.crawler.spider, "modify_realtime_request", None)
             if callable(modify_request):
