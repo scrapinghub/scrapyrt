bf31f803b18394d7edbc6ebd506f814eac88ae5b
Pawel Miech <pawelmhm@gmail.com>
2021-01-25 07:38:06 +0100

    /workflows/ build Python 3.9 too

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'Environment :: Console',
         'Environment :: No Input/Output (Daemon)',
