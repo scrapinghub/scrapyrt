235dd4a523d6ee63dcca643cb2bb1176b4bfe3b3
Pawel Miech <pawelmhm@gmail.com>
2023-08-10 10:48:03 +0200

    setup/version: bump version, drop EOL pythons

--- setup.py
+++ setup.py
@@ -25,11 +25,10 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent',
         'Environment :: Console',
         'Environment :: No Input/Output (Daemon)',
